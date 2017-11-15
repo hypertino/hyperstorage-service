8a4467e5cd4a6f4bbc1b518660bb8ffd7556625f
Maga Abdurakhmanov <maqdev@gmail.com>
2017-11-15 01:22:03 +0100

    + BSD -> MPLv2

--- src/test/scala/OrderFieldsLogicTest.scala
+++ src/test/scala/OrderFieldsLogicTest.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2017 Magomed Abdurakhmanov, Hypertino
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 import com.hypertino.binders.value.{Number, Obj}
 import com.hypertino.hyperstorage.api.{HyperStorageIndexSortFieldType, HyperStorageIndexSortItem, HyperStorageIndexSortOrder}
 import com.hypertino.hyperstorage.db._
